c10177bc8cc7d951e29eaa636b6aa1985a42684f
Prokopyuk <natali.prokopiuk@gmail.com>
2015-02-25 00:01:58 +0200

    test:Added new Triangle object

--- homework/src/lesson6/FiguresMain.java
+++ homework/src/lesson6/FiguresMain.java
@@ -12,7 +12,7 @@ public class FiguresMain {
 		
 		
 		Triangle triangle = new Triangle(2.0, 3.0);
-		
+		Triangle triangle2 = new Triangle(4.0,7.8);
 		Figure [] figs = {circle1, circle2, sqr1, triangle};
 		
 		for (Figure figure : figs) {
