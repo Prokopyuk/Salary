eed7d320ae6b6acee5d9139c5124d258b5747778
Prokopyuk <natali.prokopiuk@gmail.com>
2015-03-04 00:08:16 +0200

    Added new ArrayList

--- homework/src/employee/SalaryCount.java
+++ homework/src/employee/SalaryCount.java
@@ -1,17 +1,18 @@
 package employee;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
 
 public class SalaryCount {
 
 	public static void main(String[] args) {
-		
-		Manager manager = new Manager("Zoe",22);
-		Boss boss = new Boss("Bender",77);
-		
-		
+
+		Manager manager = new Manager("Zoe", 22);
+		Boss boss = new Boss("Bender", 77);
+
+		ArrayList <Employee>list = new ArrayList<Employee>();
+		list.add(manager);
+		list.add(boss);
+
 	}
 
 }
