32a970048edf26ad449909e51b45dd8f74aa1a12
Prokopyuk <natali.prokopiuk@gmail.com>
2015-03-10 09:15:31 +0200

    Added first  Class Employee to Firm repository

---
+++ homework/src/firmCreation/Employee.java
@@ -0,0 +1,23 @@
+package firmCreation;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+abstract public class Employee {
+
+	private String firstName;
+	private String lastName;
+	private enum gender{male, female};
+	private Date birthday; 
+	private int rate;
+	private double salaryAccount;
+	private double salary;
+	public int vactionDaysAmount;
+	public Date lastVacationDate;
+	public Date hireDay;
+	
+	
+	
+
+	    
+}
